5c49b53010dbd20d4e2c76dd5b8d6c8415e16e3f
igorlobanov <igorlobanov@gmail.com>
2022-01-15 20:36:20 +0300

    Removing some useless files

--- Telebot/Task/Update/Poll.pm
+++
@@ -1,9 +0,0 @@
-package Telebot::Task::Update::Poll;
-use Mojo::Base 'Minion::Job', -signatures;
- 
-sub run ($job, $payload, $update_id) {
-    my $app = $job->app;
-    return $job->finish('Completed');
-}
- 
-1;
